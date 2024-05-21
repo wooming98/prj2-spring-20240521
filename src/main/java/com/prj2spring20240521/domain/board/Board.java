0bd23e0d2420c222e17f7a3a718e9bcca09f6530
anwoomin <askcw39@naver.com>
2024-05-21 14:49:18 +0900

    한 게시물 보기

--- src/main/java/com/prj2spring20240521/domain/board/Board.java
+++ src/main/java/com/prj2spring20240521/domain/board/Board.java
@@ -2,10 +2,13 @@ package com.prj2spring20240521.domain.board;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Board {
     private Integer id;
     private String title;
     private String content;
     private String writer;
+    private LocalDateTime inserted;
 }
