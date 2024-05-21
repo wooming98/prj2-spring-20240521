a4a29cbc782da4b75d395ce9028243442b1f78e2
anwoomin <askcw39@naver.com>
2024-05-21 10:13:19 +0900

    최초 커밋

---
+++ src/main/java/com/prj2spring20240521/Prj2Spring20240521Application.java
@@ -0,0 +1,13 @@
+package com.prj2spring20240521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2Spring20240521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2Spring20240521Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/prj2spring20240521/Prj2Spring20240521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj2spring20240521;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2Spring20240521ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
