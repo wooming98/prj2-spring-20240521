05e09af9495b7e3fffc940e49bdc3d6faeeffe7c
anwoomin <askcw39@naver.com>
2024-05-22 12:24:50 +0900

    입력한 회원가입정보가 유효한 지 확인
    email,password,nickName 있어야함
    email pattern에 맞고
    
    "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"

--- src/main/java/com/prj2spring20240521/controller/member/MemberController.java
+++ src/main/java/com/prj2spring20240521/controller/member/MemberController.java
@@ -14,8 +14,13 @@ public class MemberController {
     private final MemberService service;
 
     @PostMapping("signup")
-    public void signup(@RequestBody Member member) {
-        service.add(member);
+    public ResponseEntity signup(@RequestBody Member member) {
+        if (service.validate(member)) {
+            service.add(member);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping(value = "check", params = "email")
--- src/main/java/com/prj2spring20240521/service/member/MemberService.java
+++ src/main/java/com/prj2spring20240521/service/member/MemberService.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberMapper mapper;
+    final MemberMapper mapper;
 
     public void add(Member member) {
         mapper.insert(member);
@@ -23,5 +23,27 @@ public class MemberService {
 
     public Member getByNickName(String nickName) {
         return mapper.selectByNickName(nickName);
+    }
+
+    public boolean validate(Member member) {
+        if (member.getEmail() == null || member.getEmail().isBlank()) {
+            return false;
+        }
+
+        if (member.getNickName() == null || member.getNickName().isBlank()) {
+            return false;
+        }
+
+        if (member.getPassword() == null || member.getPassword().isBlank()) {
+            return false;
+        }
+
+        String emailPattern = "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*";
+
+        if (!member.getEmail().trim().matches(emailPattern)) {
+            return false;
+        }
+
+        return true;
     }
 }
