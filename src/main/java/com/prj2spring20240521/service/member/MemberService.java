c30eb33ae819afadbf34587055f3dca037d91ac5
anwoomin <askcw39@naver.com>
2024-05-24 16:22:41 +0900

    회원정보 수정 후 기존 토큰 복사해서 nickName claim 업데이트 후 토큰 다시 발행/응답

--- src/main/java/com/prj2spring20240521/service/member/MemberService.java
+++ src/main/java/com/prj2spring20240521/service/member/MemberService.java
@@ -6,6 +6,7 @@ import com.prj2spring20240521.mapper.member.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -97,7 +98,7 @@ public class MemberService {
     }
 
 
-    public void modify(Member member) {
+    public Map<String, Object> modify(Member member, Authentication authentication) {
         if (member.getPassword() != null && member.getPassword().length() > 0) {
             // 패스워드가 입력되었으니 바꾸기
             member.setPassword(passwordEncoder.encode(member.getPassword()));
@@ -107,6 +108,18 @@ public class MemberService {
             member.setPassword(dbMember.getPassword());
         }
         mapper.update(member);
+
+        String token = "";
+
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        Map<String, Object> claims = jwt.getClaims();
+        JwtClaimsSet.Builder jwtClaimsSetBuilder = JwtClaimsSet.builder();
+        claims.forEach(jwtClaimsSetBuilder::claim);
+        jwtClaimsSetBuilder.claim("nickName", member.getNickName());
+
+        JwtClaimsSet jwtClaimsSet = jwtClaimsSetBuilder.build();
+        token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+        return Map.of("token", token);
     }
 
     public boolean hasAccessModify(Member member, Authentication authentication) {
