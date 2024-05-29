bbf50211819719bef834d2e3de2454acd4ad470a
anwoomin <askcw39@naver.com>
2024-05-29 19:05:11 +0900

    파일 있는 게시물 수정 (s3)

--- src/main/java/com/prj2spring20240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring20240521/service/board/BoardService.java
@@ -16,7 +16,6 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -142,10 +141,14 @@ public class BoardService {
     public void edit(Board board, List<String> removeFileList, MultipartFile[] addFileList) throws IOException {
         if (removeFileList != null && removeFileList.size() > 0) {
             for (String fileName : removeFileList) {
-                // disk의 파일 삭제
-                String path = STR."C:/Temp/prj2/\{board.getId()}/\{fileName}";
-                File file = new File(path);
-                file.delete();
+                // s3의 파일 삭제
+                String key = STR."prj2/\{board.getId()}/\{fileName}";
+                DeleteObjectRequest objectRequest = DeleteObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build();
+                s3Client.deleteObject(objectRequest);
+
                 // db records 삭제
                 mapper.deleteFileByBoardIdAndName(board.getId(), fileName);
             }
@@ -159,14 +162,16 @@ public class BoardService {
                     // 새 파일이 기존에 없을 때만 db에 추가
                     mapper.insertFileName(board.getId(), fileName);
                 }
-                // disk 에 쓰기
-                File dir = new File(STR."C:/Temp/prj2/\{board.getId()}");
-                if (!dir.exists()) {
-                    dir.mkdirs();
-                }
-                String path = STR."C:/Temp/prj2/\{board.getId()}/\{fileName}";
-                File destination = new File(path);
-                file.transferTo(destination);
+                // s3 에 쓰기
+                String key = STR."prj2/\{board.getId()}/\{fileName}";
+                PutObjectRequest objectRequest = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .acl(ObjectCannedACL.PUBLIC_READ)
+                        .build();
+
+                s3Client.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
             }
         }
 
