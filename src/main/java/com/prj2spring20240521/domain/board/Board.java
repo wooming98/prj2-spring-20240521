0d74e25ccb74b7b703854d262d1e3b8ef6bae166
anwoomin <askcw39@naver.com>
2024-05-30 10:09:26 +0900

    게시물 목록에 좋아요 갯수(서브 쿼리)

--- src/main/java/com/prj2spring20240521/domain/board/Board.java
+++ src/main/java/com/prj2spring20240521/domain/board/Board.java
@@ -14,6 +14,7 @@ public class Board {
     private Integer memberId;
     private LocalDateTime inserted;
 
+    private Integer numberOfLike;
     private Integer numberOfImages;
     private List<BoardFile> fileList;
 }
