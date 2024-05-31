5dcf4ea56bc11f3b8af740913bc56de92743db4c
anwoomin <askcw39@naver.com>
2024-05-31 11:30:11 +0900

    댓글 작성 시간 보기 수정
    getInserted 메소드 추가
    현재기준 24시간 내면 시간만
    그 이전이면 날짜만

--- src/main/java/com/prj2spring20240521/domain/comment/Comment.java
+++ src/main/java/com/prj2spring20240521/domain/comment/Comment.java
@@ -3,6 +3,7 @@ package com.prj2spring20240521.domain.comment;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class Comment {
@@ -13,4 +14,17 @@ public class Comment {
     private LocalDateTime inserted;
 
     private String nickName; // 작성자 별명
+
+    public String getInserted() {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+
+        if (inserted.isBefore(beforeOneDay)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return inserted.format(formatter).toString();
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            return inserted.format(formatter).toString();
+        }
+
+    }
 }
